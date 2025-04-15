4cccade461629d65efbf221f9b58d4e341c600f2
chikwanda <your_github_email@example.com>
2025-04-15 15:58:00 -0400

    added stuff to main.py and auth.py

---
+++ auth.py
@@ -0,0 +1,38 @@
+import json
+from getpass import getpass
+class Auth:
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        master_password1 = getpass("Setup your master password: ")
+        master_password2 = getpass("Confirm your master password: ")
+        # Check if the passwords match
+        while master_password1 != master_password2:
+            print("Passwords do not match")
+            master_password1 = getpass("Setup your master password: ")
+            master_password2 = getpass("Confirm your master password: ")
+            
+        
+        with open('vault.json', 'w') as vault_file:
+            json.dump({'master_pwd': master_password1}, vault_file)
+    
+    def password_exists(self):
+        with open('vault.json', 'r') as vault_file:
+            vault_data = json.load(vault_file)
+        if 'master_pwd' in vault_data:
+            return True
+        else:
+            return False
+    
+    def login(self):
+        master_password = getpass("Enter your master password: ")
+        with open('vault.json', 'r') as vault_file:
+            vault_data = json.load(vault_file)
+        if self.password_exists():
+            if vault_data['master_pwd'] == master_password:
+                return True
+            else:
+                return False
+        else:
+            return False
